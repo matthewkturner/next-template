be9f11af9e196459e6ff171856f24280fdb7765b
Matthew K. Turner <matthew.keith.turner@gmail.com>
2020-02-07 16:51:27 -0800

    Add eslint config.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import React from 'react';
+
 const Index = () => {
-  return <div>Hello world!</div>;
+  return (
+    <main>
+      <img src="/logo.png" />
+      <div>
+        <h1>Hello world!</h1>
+        <p>Hi hi, hello.</p>
+      </div>
+    </main>
+  );
 };
 
 export default Index;
